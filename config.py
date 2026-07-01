9a11fd2da9bdd09410f4e6b14a6a36f20d02bd69
vopopyanap <vopopyanap@gmail.com>
2026-07-01 20:36:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-BOT_TOKEN = "8708080868:AAHDyCUbZfqZ5KiVpeHdayFMv_-YbGG6wHI"
-ADMIN_IDS = [7828604612]  # список ID администраторов
 
-# ЮKassa (тестовые или боевые ключи)
-YOOKASSA_SHOP_ID = "1358857"
-YOOKASSA_SECRET_KEY = "live__1Ak-c9fC4KLjVuJntcMCBVBaFHo_f0kYNvwKoA9xLw"  # для тестов test_, для боя live_
